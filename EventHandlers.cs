b393f8464d3fe3be7c6d2510e1de04973c83dff9
Cory Rylan <splintercode.cb@gmail.com>
2022-02-05 10:48:14 -0600

    chore: dynamic type example
    Signed-off-by: Cory Rylan <splintercode.cb@gmail.com>

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -6,25 +6,31 @@
  */
 
 using Microsoft.AspNetCore.Components;
+using System.Text.Json;
 
 namespace clarity_blazor;
 
-[EventHandler("oncloseChange", typeof(CustomEventArgs<Object>))] // cds-alert: notify when the user has clicked the dismiss button
-[EventHandler("onexpandedChange", typeof(CustomEventArgs<Object>))] // cds-accordion-panel: notify when the user has clicked the panel header
-[EventHandler("oncloseChange", typeof(CustomEventArgs<Object>))] // cds-dropdown: Notify user when close event has occurred
-[EventHandler("oncloseChange", typeof(CustomEventArgs<Object>))] // cds-modal: notify when the user has clicked the close button
-[EventHandler("onexpandedChange", typeof(CustomEventArgs<Object>))] // cds-navigation-group: notify when the user has clicked the navigation expand/collapse button
-[EventHandler("onexpandedChange", typeof(CustomEventArgs<Object>))] // cds-navigation: notify when the user has clicked the navigation expand/collapse button
-[EventHandler("onexpandedChange", typeof(CustomEventArgs<Object>))] // cds-tree-item: notify when the user has clicked the expand / collapse button
-[EventHandler("onselectedChange", typeof(CustomEventArgs<Object>))] // cds-tree-item: notify when the user has clicked the tree item
-[EventHandler("oncloseChange", typeof(CustomEventArgs<Object>))] // cds-internal-overlay: Notify user when close event has occurred
-[EventHandler("oncloseChange", typeof(CustomEventArgs<Object>))] // cds-internal-popup: Notify user when close event has occurred
+[EventHandler("oncloseChange", typeof(CustomEventArgs))] // cds-alert: notify when the user has clicked the dismiss button
+[EventHandler("onexpandedChange", typeof(CustomEventArgs))] // cds-accordion-panel: notify when the user has clicked the panel header
+[EventHandler("oncloseChange", typeof(CustomEventArgs))] // cds-dropdown: Notify user when close event has occurred
+[EventHandler("oncloseChange", typeof(CustomEventArgs))] // cds-modal: notify when the user has clicked the close button
+[EventHandler("onexpandedChange", typeof(CustomEventArgs))] // cds-navigation-group: notify when the user has clicked the navigation expand/collapse button
+[EventHandler("onexpandedChange", typeof(CustomEventArgs))] // cds-navigation: notify when the user has clicked the navigation expand/collapse button
+[EventHandler("onexpandedChange", typeof(CustomEventArgs))] // cds-tree-item: notify when the user has clicked the expand / collapse button
+[EventHandler("onselectedChange", typeof(CustomEventArgs))] // cds-tree-item: notify when the user has clicked the tree item
+[EventHandler("oncloseChange", typeof(CustomEventArgs))] // cds-internal-overlay: Notify user when close event has occurred
+[EventHandler("oncloseChange", typeof(CustomEventArgs))] // cds-internal-popup: Notify user when close event has occurred
 public static class EventHandlers
 {
 
 }
 
-public class CustomEventArgs<T> : EventArgs
+public class CustomEventArgs : EventArgs
 {
-  public T? Detail { get; set; }
+  public dynamic? Detail { get; set; }
+
+  /* Returns the detail value of CustomEvent with given type */
+  public T GetDetail<T>() {
+    return JsonSerializer.Deserialize<T>(Detail); // used to cast dynamic type, unknown until event occurs at runtime
+  }
 }
