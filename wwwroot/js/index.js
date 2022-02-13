94e588e267a50e732a1b253b0a53e4326d0d7e25
Cory Rylan <splintercode.cb@gmail.com>
2022-02-13 12:13:06 -0600

    chore: dependency updates
    Signed-off-by: Cory Rylan <splintercode.cb@gmail.com>

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,2 +1,2 @@
-import('https://cdn.skypack.dev/@cds/core@v6.0.0-next.3/button/register.js');
-import('https://cdn.skypack.dev/@cds/core@v6.0.0-next.3/alert/register.js');
+import('https://cdn.skypack.dev/pin/@cds/core@v6.0.0-next.4-QVPeIoBWPyUxeVUrYlUP/mode=imports,min/optimized/@cds/core/button/register.js');
+import('https://cdn.skypack.dev/pin/@cds/core@v6.0.0-next.4-QVPeIoBWPyUxeVUrYlUP/mode=imports,min/optimized/@cds/core/alert/register.js');
